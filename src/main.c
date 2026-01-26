cada1e304bc13753fcab948d917340c532034aaa
Gabriel Carlsson <gabriel-carlsson@hotmail.se>
2026-01-26 14:59:51 +0100

    feat: send votes to ThingSpeak on button press

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
-
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "vote_manager.h"
 #include "wifi_driver.h"
 #include "button_driver.h"
 #include "lcd_driver.h"
+#include "thingspeak_driver.h"
+
+#define LESSON_ID 1 // Välj ID för lektionen
 
 static void update_lcd(void) {
     VoteResults r = get_results();
@@ -20,17 +22,32 @@ static void update_lcd(void) {
     lcd_driver_set_cursor(0,1);
     lcd_driver_print(buf);
 }
+// Skapar en funktion som lagrar rösten och skickar sedan direkt till molnet
+static void handle_vote(int voteValue) {
+   
+    // Spara rösten lokalt
+    add_vote(voteValue);
+
+    // Skicka den vidare tillsammans med lektions ID till ThingSpeak 
+    (void)send_vote_to_thingspeak(LESSON_ID, voteValue);
+    
+    // Skriv ut meddelande och uppdatera LCD:n
+    printf("Tack för din röst\n");
+    update_lcd();
+
+}
 
 int main(void) {
     stdio_init_all();
 
-    // WiFi
+    // Innan vi anropar funktionerna
+    // Anslut Pico W till WiFi
     if (!wifi_driver_init()) {
         printf("WiFi kunde inte anslutas. Offline-läge.\n");
     } else {
         printf("WiFi anslutet! Pico W är online.\n");
     }
-
+    // initiera funktioner för rösthantering, knappar och LCD
     vote_manager_init();
     button_driver_init();
     lcd_driver_init();
@@ -39,24 +56,23 @@ int main(void) {
 
     while (true) {
         if (button_driver_was_pressed(BUTTON_RED)) {
-            add_vote(VOTE_RED);
+            handle_vote(VOTE_RED);
             printf("Red button pressed\n");
-            update_lcd();
         }
 
         if (button_driver_was_pressed(BUTTON_YELLOW)) {
-            add_vote(VOTE_YELLOW);
+            handle_vote(VOTE_YELLOW);
             printf("Yellow button pressed\n");
-            update_lcd();
         }
 
         if (button_driver_was_pressed(BUTTON_GREEN)) {
-            add_vote(VOTE_GREEN);
+            handle_vote(VOTE_GREEN);
             printf("Green button pressed\n");
-            update_lcd();
         }
-
-        sleep_ms(50); // enkel debounce
+     
+        // förhindra att flera röster registreras vid ett knapptryck
+        sleep_ms(50); 
     }
+
 }
 
